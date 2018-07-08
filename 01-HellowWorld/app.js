ce3e690dc6ee191e28106ec83b7c367ba3d445c3
ayyalu <ayyalu@gmail.com>
2018-07-08 11:11:18 +0530

    Commit

---
+++ 01-HellowWorld/app.js
@@ -0,0 +1,7 @@
+app.use('/web', express.static(__dirname + '/web'));
+var server = app.listen(8080, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
